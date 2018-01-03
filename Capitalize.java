2f7a5f9a63a625192e58e7b62511ac8ce3ae7963
Raj_Yadav <raj.yadav@aot.edu.in>
2018-01-03 12:48:37 +0530

    Letter Changes
    CoderByte Problem

--- Capitalize.java
+++ Capitalize.java
@@ -4,19 +4,17 @@ public class Capitalize {
 
 	public static void main(String[] args) {
 
-		int i,j;
+		int i;
 		Scanner sc=new Scanner(System.in);
 		String s=sc.nextLine();
-		char []ch=s.toCharArray();
+		char[]ch=s.toCharArray();
 		for(i=0;i<ch.length;i++){
-			if(ch[i]>='A'&&ch[i]<='z')
-			if(ch[i]+1=='a' || ch[i]+1=='e'||ch[i]+1=='i'||ch[i]+1=='o' ||ch[i]+1=='u'){
-				ch[i]+=1;
+			if(i==0){
 				ch[i]-=32;
-			}else{
-				ch[i]+=1;
 			}
-			
+			if(ch[i]==' '){
+				ch[i+1]-=32;
+			}
 		}
 		String ns=new String(ch);
 		System.out.println(ns);
