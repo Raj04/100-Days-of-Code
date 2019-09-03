f2be694921d91e32bed478d743afba8d0a7e95ee
Raj_Yadav <rajjoinsall@gmail.com>
2019-09-03 21:49:35 +0530

    Subset Problem

--- Subset.java
+++ Subset.java
@@ -6,18 +6,9 @@ import java.util.*;
 public class Subset {
 	
 	public static boolean CheckSubset(int[] arr1, int n1, int[] arr2, int n2) {
-		/*Your class should be named solution. 
-		*Don't write main().
-		*Don't take input, it is passed as function argument.
-		*Don't print output.
-		*Taking input and printing output is handled automatically.
-		*/ 
-//         if(n1==0 || n2==0){
-//             return false;
-//         }
+	
         int counter=0;
-//         boolean flag=false;
-        Set<Integer> set=new HashSet<>();
+	Set<Integer> set=new HashSet<>();
         for(int i=0;i<n1;i++){
             set.add(arr1[i]);
         }
@@ -26,7 +17,16 @@ public class Subset {
                 counter++;
             }
         }
-        if(counter==n2){
+	//we have to compare with set size() as the no of elements matched with the first has to be same 
+	//as well as in the number of count also
+	/*3
+	8 18 8
+	3 
+	8 8 8 --->in this 8 is matched but it is not a subset as the no of 8 count is not same
+	in the first its only 2 other one has 3 instead, so we have to keep a track of count also
+	since HashSet contains only unique so we compare with the size() to check exact subset is there or not.
+	*/
+        if(counter==set.size()){
             return true;
         }
         return false;
