0d74f7b1d5e437218f20975dcdc189d5ff9cae67
Raj_Yadav <rajjoinsall@gmail.com>
2019-09-24 10:15:52 +0530

    Add One to a number

---
+++ Bit Manipulation/AddOne.java
@@ -0,0 +1,15 @@
+//Link: adding one to a number without using any airthmetic operator
+//we use bitwise or for this problem
+
+/*package whatever //do not write package name here */
+
+import java.util.*;
+
+class AddOne {
+	public static void main (String[] args) {
+	    
+	    //adding one to a number without using any airthmetic operator
+	    int x=8;
+	    System.out.println(x|1);
+	}
+}
